a2265f700fd99bd23f5cf1defbc95f37c39fbea4
Francisco Chaves <francisco.jschaves@gmail.com>
2020-04-20 18:09:13 -0300

    Use text files to store program names

--- src/software-snap.sh
+++ src/software-snap.sh
@@ -7,30 +7,13 @@ sudo systemctl start snapd.service
 
 sleep 3
 
-SOFTWARE_TO_INSTALL_SNAP_CLASSIC=('android-studio'
-                                'eclipse'
-                                'netbeans'
-                                'code'
-                                'pycharm-community')
+SOFTWARE_TO_INSTALL_SNAP_CLASSIC=$(cat ./softwares/software-snap-classic.txt)
 
 for software in ${SOFTWARE_TO_INSTALL_SNAP_CLASSIC[@]}; do
     sudo snap install $software --classic
 done
 
-SOFTWARE_TO_INSTALL_SNAP=('gitkraken'
-                        'dbeaver-ce'
-                        'doctl'
-                        'inkscape'
-                        'gimp'
-                        'polar-bookshelf'
-                        'buka'
-                        'vlc'
-                        'kdenlive'
-                        'obs-studio'
-                        'audacity'
-                        'telegram-desktop'
-                        'ktouch')
-
+SOFTWARE_TO_INSTALL_SNAP=$(cat ./softwares/software-snap.txt)
 
 for software in ${SOFTWARE_TO_INSTALL_SNAP[@]}; do
     sudo snap install $software
