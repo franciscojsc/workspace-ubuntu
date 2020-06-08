d9d3da52ad7a3f2111e665983f4bc61defef99ea
Francisco Chaves <francisco.jschaves@gmail.com>
2020-06-08 20:25:17 -0300

    Adjust script for Internet connection

--- src/check-net.sh
+++ src/check-net.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
-ping www.google.com -c 5 2>&1
-if [ $? -eq 0 ]
-then
-    echo ' [OK] Connection Internet'
-else
-    echo ' [  ] Connection Internet'
-    exit 1
-fi;
+CONT_NET=0
+
+checkNet() {
+    ping www.google.com -c 5 > /dev/null 2>&1 
+    if [ $? -eq 0 ]
+    then
+        echo  -e "${COLOR_SUCESS} ✅ ${COLOR_DEFAULT} Connection Internet"
+    else
+        if [ ${CONT_NET} -eq 10 ]
+        then
+            echo
+            echo  -e "${COLOR_DANGER} ╦┌┐┌┌┬┐┌─┐┬─┐┌┐┌┌─┐┌┬┐  ┌─┐┌─┐┌┐┌┌┐┌┌─┐┌─┐┌┬┐┬┌─┐┌┐┌  ┌─┐┌─┐┬┬  ┌─┐┌┬┐"
+            echo  -e "${COLOR_DANGER} ║│││ │ ├┤ ├┬┘│││├┤  │   │  │ │││││││├┤ │   │ ││ ││││  ├┤ ├─┤││  ├┤  ││"
+            echo  -e "${COLOR_DANGER} ╩┘└┘ ┴ └─┘┴└─┘└┘└─┘ ┴   └─┘└─┘┘└┘┘└┘└─┘└─┘ ┴ ┴└─┘┘└┘  └  ┴ ┴┴┴─┘└─┘─┴┘"
+            echo
+            exit 1
+        fi
+        echo  -e "${COLOR_DANGER} ❌ ${COLOR_DEFAULT} Connection Internet"
+        CONT_NET=$(($CONT_NET+1))
+        sleep 3
+        checkNet
+    fi;
+}
+
+checkNet
