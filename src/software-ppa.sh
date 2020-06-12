edce483518e1ec0a65931ab8b685f9deb5111c72
Francisco Chaves <francisco.jschaves@gmail.com>
2020-06-12 17:49:45 -0300

    Add verification to install ppa programs

--- src/software-ppa.sh
+++ src/software-ppa.sh
@@ -3,18 +3,27 @@
 # Softwares ppa
 
 # Install Peek
-sudo add-apt-repository --yes ppa:peek-developers/stable
-sudo apt-get update 
-sudo apt-get -y install peek
+if [ -z $(which peek) ]
+then
+    sudo add-apt-repository --yes ppa:peek-developers/stable
+    sudo apt-get update 
+    sudo apt-get -y install peek
+fi
 
 # Install VeraCrypt
-sudo add-apt-repository --yes ppa:unit193/encryption
-sudo apt-get update
-sudo apt-get -y install veracrypt
+if [ -z $(which veracrypt) ]
+then
+    sudo add-apt-repository --yes ppa:unit193/encryption
+    sudo apt-get update
+    sudo apt-get -y install veracrypt
+fi
 
 # VirtualBox
-sudo echo "deb [arch=amd64] https://download.virtualbox.org/virtualbox/debian $(lsb_release -sc) contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
-wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
-wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install -y virtualbox-6.1
+if [ -z $(which virtualbox) ]
+then
+    sudo echo "deb [arch=amd64] https://download.virtualbox.org/virtualbox/debian $(lsb_release -sc) contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
+    wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+    wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+    sudo apt-get update
+    sudo apt-get install -y virtualbox-6.1
+fi
