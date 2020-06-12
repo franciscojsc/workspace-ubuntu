44ed982b04e48adffcfb1b8c052a82be75dfeed5
Francisco Chaves <francisco.jschaves@gmail.com>
2020-06-12 17:55:45 -0300

    Add verification to install programs by scripts

--- src/software-script.sh
+++ src/software-script.sh
@@ -1,11 +1,39 @@
 #!/bin/bash
 
-# Install oh-my-zsh
-sudo curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh
+installSoftwareScript() {
+    if [ ! -z $(which curl) ]
+    then
+        # Install oh-my-zsh
+        if [ ! -d "$HOME/.oh-my-zsh" ]
+        then
+            if [ ! -z $(which git) ]
+            then
+                sudo curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh | bash
+            else
+                echo -e "${COLOR_WARNING} Install git: ${COLOR_DEFAULT}"
+                sudo apt-get -y install git
+                installSoftwareScript
+            fi
+        fi
 
-# Install docker
-sudo curl -fsSL https://get.docker.com | sh
-sudo usermod -aG docker $USER 
+        # Install NVM
+        if [ ! -d "$HOME/.nvm" ]
+        then
+            sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
+            # nvm install node
+        fi
 
-# Install NVM
-sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
+        # Install docker
+        if [ -z $(which docker) ]
+        then
+            sudo curl -fsSL https://get.docker.com | bash
+            sudo usermod -aG docker $USER
+        fi
+    else
+        echo -e "${COLOR_WARNING} Install curl: ${COLOR_DEFAULT}"
+        sudo apt-get -y install curl
+        installSoftwareScript
+    fi
+}
+
+installSoftwareScript
