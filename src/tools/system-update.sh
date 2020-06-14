4992ed87fbf688e04a79c6126e84ffdd93c9f66f
Francisco Chaves <francisco.jschaves@gmail.com>
2020-06-14 14:28:23 -0300

    Check if the user has sudo privileges

--- src/tools/system-update.sh
+++ src/tools/system-update.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-(sudo apt-get update && \
-sudo apt-get -y dist-upgrade && \
-sudo apt-get -y clean && \
-sudo apt-get -y autoremove && \
-sudo apt-get -y autoclean && \
-sudo dpkg --configure -a
-) > /dev/null 2>&1 &
+if [ -z $(sudo -v || echo "not-sudo") ]
+then
+    (sudo apt-get update && \
+    sudo apt-get -y dist-upgrade && \
+    sudo apt-get -y clean && \
+    sudo apt-get -y autoremove && \
+    sudo apt-get -y autoclean && \
+    sudo dpkg --configure -a
+    ) > /dev/null 2>&1 &
+fi
