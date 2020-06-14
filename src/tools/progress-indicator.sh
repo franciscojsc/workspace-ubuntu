b0e7f8c977e9159da3728ad2322953db8e80f1b8
Francisco Chaves <francisco.jschaves@gmail.com>
2020-06-14 14:13:33 -0300

    Check more than one task in the background

--- src/tools/progress-indicator.sh
+++ src/tools/progress-indicator.sh
@@ -4,25 +4,24 @@
 # SOURCE: <https://github.com/lnfnunes/bash-progress-indicator>
 # LICENSE: <https://github.com/lnfnunes/bash-progress-indicator/blob/master/LICENSE>
 # MODIFIED BY: Francisco Chaves <https://github.com/franciscojsc>
+# DESCRIPTION: While there are processes in the background, the progress bar is displayed
 
 progress() {
-
-    FRAMES=("⠋" "⠙" "⠹" "⠸" "⠼" "⠴" "⠦" "⠧" "⠇" "⠏")
-    FRAME_INTERVAL=0.1
-
-    tput civis -- invisible
-
-    for FRAME in "${FRAMES[@]}"; do
-        echo -ne "\\r $FRAME "
-        sleep $FRAME_INTERVAL
-    done
-
-    if [ -z $(jobs -pr) ]
+    if [ $(jobs -pr | wc -l) != 0 ]
     then
-        echo -ne "\\r   \n"
-        tput cnorm -- normal
-    else
+        FRAMES=("⠋" "⠙" "⠹" "⠸" "⠼" "⠴" "⠦" "⠧" "⠇" "⠏")
+        FRAME_INTERVAL=0.1
+
+        tput civis -- invisible
+
+        for FRAME in "${FRAMES[@]}"; do
+            echo -ne "\\r $FRAME "
+            sleep $FRAME_INTERVAL
+        done
         progress
+    else
+        echo -ne "\\r ✅ \n"
+        tput cnorm -- normal
     fi
 }
 
