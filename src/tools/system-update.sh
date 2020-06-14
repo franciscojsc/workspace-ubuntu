9bc39b4df1044b11761cea4947a304348df22d68
Francisco Chaves <francisco.jschaves@gmail.com>
2020-06-14 14:25:21 -0300

    Group the system update commands

--- src/tools/system-update.sh
+++ src/tools/system-update.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-sudo apt-get update > /dev/null 2>&1 &
-sudo apt-get -y dist-upgrade > /dev/null 2>&1 &
-sudo apt-get -y clean > /dev/null 2>&1 &
-sudo apt-get -y autoremove > /dev/null 2>&1 &
-sudo apt-get -y autoclean > /dev/null 2>&1 &
-sudo dpkg --configure -a > /dev/null 2>&1 &
+(sudo apt-get update && \
+sudo apt-get -y dist-upgrade && \
+sudo apt-get -y clean && \
+sudo apt-get -y autoremove && \
+sudo apt-get -y autoclean && \
+sudo dpkg --configure -a
+) > /dev/null 2>&1 &
